ae96bf990507f674fa9893ba3a14b09efd4c5a17
Aaron <a.genevision14@gmail.com>
2024-04-24 09:20:21 -0500

    Developed db model and to json function

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
 # Main routes of applications
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+# Create
+# Needs first and last name and email
+
+
+# Read
+
+# Update
+
+# Delete
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,18 @@
 # Database models
+
+from config import db
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    # Takes above fields and creates a python dictionary, which can be converted into JSON, which can then be pass from our API
+    def to_json(self):
+        return { 
+            'id': self.id, 
+            'firstName': self.first_name,  
+            'lastName': self.last_name,  
+            'email': self.email 
+        }
